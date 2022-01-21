8f2e706645c4945dcbcf1fc955443cf768c0428b
ivana0905 <74403847+ivana0905@users.noreply.github.com>
2022-01-21 14:36:38 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -64,7 +64,7 @@ export default {
   transform: translate(-50%,-50%);
   left: 50%;
   top: 50%;
-  color: white;
+  color: pink;
 }
 .background .shape{
   height: 200px;
@@ -76,20 +76,20 @@ export default {
 form{
   height: 420px;
   width: 400px;
-  background-color: rgba(255,255,255,0.13);
+  background-color: pink;
   position: absolute;
   transform: translate(-50%,-50%);
   top: 50%;
   left: 50%;
   border-radius: 10px;
   backdrop-filter: blur(10px);
-  border: 2px solid rgba(255,255,255,0.1);
-  box-shadow: 0 0 40px rgba(8,7,16,0.6);
+  border: 2px solid palevioletred;
+  box-shadow: 0 0 40px white;
   padding: 50px 35px;
 }
 form *{
   font-family: 'Poppins',sans-serif;
-  color: #000000;
+  color: white;
   letter-spacing: 0.5px;
   outline: none;
   border: none;
@@ -106,13 +106,13 @@ label{
   margin-top: 30px;
   font-size: 16px;
   font-weight: 500;
-  color: #000000;
+  color: white;
 }
 input{
   display: block;
   height: 50px;
   width: 100%;
-  background-color: rgba(3, 3, 3, 0.07);
+  background-color: lightpink;
   border-radius: 3px;
   padding: 0 10px;
   margin-top: 8px;
@@ -120,13 +120,13 @@ input{
   font-weight: 300;
 }
 ::placeholder{
-  color: #e5e5e5;
+  color: white;
 }
 .btn-login{
   margin-top: 70px;
   width: 100%;
-  background: linear-gradient(white, #989894, white);
-  color: #080710;
+  background: linear-gradient(pink, palevioletred, pink);
+  color: white;
   padding: 15px 0;
   font-size: 18px;
   font-weight: 600;
@@ -146,7 +146,7 @@ input{
   text-align: center;
 }
 .social div:hover{
-  background-color: rgba(255,255,255,0.47);
+  background-color: white;
 }
 .social .fb{
   margin-left: 25px;
